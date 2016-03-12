6262e91df56deb1ceb5ff0949686af1a17863181
Suguru YAMAMOTO <sgryjp@gmail.com>
2016-03-13 01:19:44 +0900

    cursorNextWordEndJa and cursorPrevWordStartJa
    Not finished yet.

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,148 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+//-------------------------------------------------------------------------------------------------
 export function activate(context: vscode.ExtensionContext) {
+    let command;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "japanese-word-handler" is now active!');
+    command = vscode.commands.registerCommand('extension.cursorNextWordEndJa', () => {
+        cursorNextWordEndJa(vscode.window.activeTextEditor);
+    });
+    context.subscriptions.push(command);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+    command = vscode.commands.registerCommand('extension.cursorPrevWordStartJa', () => {
+        cursorPrevWordStartJa(vscode.window.activeTextEditor);
+    });
+    context.subscriptions.push(command);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+//-------------------------------------------------------------------------------------------------
+export function cursorNextWordEndJa(editor: vscode.TextEditor) {
+    let pos = positionOfNextWordEnd(editor.document, caretPositionOf(editor));
+    editor.selections = [new vscode.Selection(pos, pos)];
+}
+
+export function cursorPrevWordStartJa(editor: vscode.TextEditor) {
+    let pos = positionOfPrevWordStart(editor.document, caretPositionOf(editor));
+    editor.selections = [new vscode.Selection(pos, pos)];
+}
+
+//-------------------------------------------------------------------------------------------------
+enum CharClass {
+    Alnum,
+    Whitespace,
+    Punctuation,
+    Hiragana,
+    Katakana,
+    Other,
+    Invalid
+}
+
+function isPrevCharIsSpace(doc: vscode.TextDocument, position: vscode.Position) {
+    let prevPos = doc.positionAt( doc.offsetAt(position) - 1 );
+    return (classifyChar(doc, prevPos) == CharClass.Whitespace);
+}
+
+function caretPositionOf(editor: vscode.TextEditor) {
+    return editor.selection.isReversed ? editor.selection.start
+                                       : editor.selection.end;
+}
+
+function positionOfNextWordEnd(doc: vscode.TextDocument, caretPos: vscode.Position) {
+    // Firstly skip a series of whitespaces, or a series of EOL codes. 
+    let pos = caretPos;
+    let klass = classifyChar(doc, pos);
+    if( klass == CharClass.Whitespace )
+    {
+        do {
+            // Intentionally avoiding to use doc.positionAt(doc.offsetAt())
+            // so that the seek stops at the EOL.
+            pos = new vscode.Position( pos.line, pos.character + 1 );
+        }
+        while( classifyChar(doc, pos) == CharClass.Whitespace );
+    }
+    else if( klass == CharClass.Invalid )
+    {
+        if(pos.line + 1 < doc.lineCount) {
+            return new vscode.Position(pos.line + 1, 0); // Beginning of the next line. 
+        }
+        else {
+            return pos; // Already at the EOF.
+        }
+    }
+
+    // Then, seek until the character type changes.
+    const initKlass = classifyChar(doc, pos);
+    let nextPos = doc.positionAt(doc.offsetAt(pos) + 1);
+    while( nextPos.isAfter(pos) ) {
+        if( initKlass != classifyChar(doc, pos) ) {
+            break;
+        }
+
+        pos = nextPos;
+        nextPos = doc.positionAt(doc.offsetAt(pos) + 1);
+    }
+
+    return pos;
+}
+
+function positionOfPrevWordStart(doc: vscode.TextDocument, caretPos: vscode.Position) {
+    // Firstly skip whitespaces, excluding EOL codes. 
+    let pos = caretPos;
+    while( isPrevCharIsSpace(doc, pos) ) {
+        // Intentionally avoiding to use doc.positionAt(doc.offsetAt())
+        // so that the seek stops at the EOL.
+        pos = new vscode.Position( pos.line, pos.character - 1 );
+    }
+
+    // Then, seek until the character type changes.
+    pos = doc.positionAt(doc.offsetAt(pos) - 1);
+    const initKlass = classifyChar(doc, pos);
+    let prevPos = doc.positionAt(doc.offsetAt(pos) - 1);
+    while( prevPos.isBefore(pos) ) {
+        if( initKlass != classifyChar(doc, prevPos) ) {
+            break;
+        }
+
+        pos = prevPos;
+        prevPos = doc.positionAt(doc.offsetAt(pos) - 1);
+    }
+
+    return pos;
+}
+
+function classifyChar(doc: vscode.TextDocument, position: vscode.Position) {
+    let range = new vscode.Range(position.line, position.character,
+                            position.line, position.character + 1);
+    let text = doc.getText(range);
+    if( text.length == 0 )
+        return CharClass.Invalid;           // beyond an end-of-line / an end-of-document.
+    let ch = text.charCodeAt(0);
+
+    if( (0x09 <= ch && ch <= 0x0d) || ch == 0x20 || ch == 0x3000 )
+        return CharClass.Whitespace;
+
+    if( (0x30 <= ch && ch <= 0x39)          // halfwidth digit
+        || (0xff10 <= ch && ch <= 0xff19)   // fullwidth digit
+        || (0x41 <= ch && ch <= 0x5a)       // halfwidth alphabet, upper case
+        || ch == 0x5f                       // underscore
+        || (0x61 <= ch && ch <= 0x7a)       // halfwidth alphabet, lower case
+        || (0xc0 <= ch && ch <= 0xff        // latin character 
+            && ch != 0xd7 && ch != 0xf7)    // (excluding multiplication/division sign)
+        || (0xff21 <= ch && ch <= 0xff3a)   // fullwidth alphabet, upper case
+        || ch == 0xff3f                     // fullwidth underscore
+        || (0xff41 <= ch && ch <= 0xff5a) ) // fullwidth alphabet, lower case
+        return CharClass.Alnum;
+
+    if( 0x09 <= ch && ch <= 0x0d )          // fullwidth hiragana
+        return CharClass.Hiragana;
+
+    if( (0x30a0 <= ch && ch <= 0x30ff)      // fullwidth katakana
+        && ch != 0x30fb )                   // excluding katakana middle dot
+        return CharClass.Katakana;
+
+    if( 0xff66 <= ch && ch <= 0xff9d )      // halfwidth katakana
+        return CharClass.Katakana;
+
+    return CharClass.Other;
 }
--- test/extension.test.ts
+++ test/extension.test.ts
@@ -11,12 +11,89 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
 
+function setText(editor:vscode.TextEditor, text:string) {
+    return editor.edit((editBuilder: vscode.TextEditorEdit)=>{
+        const doc = editor.document;
+        const startPos = new vscode.Position(0, 0);
+        const lastLine = doc.lineAt( doc.lineCount - 1 );
+        const endPos = lastLine.range.end;
+        const entireRange = new vscode.Range(startPos, endPos);
+        editBuilder.replace(entireRange, text);
+    });
+}
+
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", () => {
 
-	// Defines a Mocha unit test
-	test("Something 1", () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
-	});
+    test("cursorNextWordEndJa", () => {
+        const editor = vscode.window.activeTextEditor;
+
+        setText(editor, "aB_ \tＣd＿ あいアイ相愛").then((success:boolean) => {
+            editor.selections = [new vscode.Selection(0, 0, 0, 0)];
+            myExtension.cursorNextWordEndJa(editor);
+            assert.equal(3, editor.selection.active.character);
+
+            editor.selections = [new vscode.Selection(0, 3, 0, 3)];
+            myExtension.cursorNextWordEndJa(editor);
+            assert.equal(8, editor.selection.active.character);
+
+            editor.selections = [new vscode.Selection(0, 4, 0, 4)];
+            myExtension.cursorNextWordEndJa(editor);
+            assert.equal(8, editor.selection.active.character);
+
+            editor.selections = [new vscode.Selection(0, 8, 0, 8)];
+            myExtension.cursorNextWordEndJa(editor);
+            assert.equal(11, editor.selection.active.character);
+
+            editor.selections = [new vscode.Selection(0, 11, 0, 11)];
+            myExtension.cursorNextWordEndJa(editor);
+            assert.equal(13, editor.selection.active.character);
+
+            editor.selections = [new vscode.Selection(0, 13, 0, 13)];
+            myExtension.cursorNextWordEndJa(editor);
+            assert.equal(15, editor.selection.active.character);
+
+            editor.selections = [new vscode.Selection(0, 15, 0, 15)];
+            myExtension.cursorNextWordEndJa(editor);
+            assert.equal(15, editor.selection.active.character);
+        });
+    });
+
+    test("cursorPrevWordStartJa", () => {
+        const editor = vscode.window.activeTextEditor;
+
+        setText(editor, "aB_ \tＣd＿ あいアイ相愛").then((success:boolean) => {
+            editor.selections = [new vscode.Selection(0, 0, 0, 0)];
+            myExtension.cursorPrevWordStartJa(editor);
+            assert.equal(0, editor.selection.active.character);
+
+            editor.selections = [new vscode.Selection(0, 4, 0, 4)];
+            myExtension.cursorPrevWordStartJa(editor);
+            assert.equal(0, editor.selection.active.character);
+
+            editor.selections = [new vscode.Selection(0, 5, 0, 5)];
+            myExtension.cursorPrevWordStartJa(editor);
+            assert.equal(0, editor.selection.active.character);
+
+            editor.selections = [new vscode.Selection(0, 6, 0, 6)];
+            myExtension.cursorPrevWordStartJa(editor);
+            assert.equal(5, editor.selection.active.character);
+
+            editor.selections = [new vscode.Selection(0, 9, 0, 9)];
+            myExtension.cursorPrevWordStartJa(editor);
+            assert.equal(5, editor.selection.active.character);
+
+            editor.selections = [new vscode.Selection(0, 10, 0, 10)];
+            myExtension.cursorPrevWordStartJa(editor);
+            assert.equal(9, editor.selection.active.character);
+
+            editor.selections = [new vscode.Selection(0, 12, 0, 12)];
+            myExtension.cursorPrevWordStartJa(editor);
+            assert.equal(11, editor.selection.active.character);
+
+            editor.selections = [new vscode.Selection(0, 15, 0, 15)];
+            myExtension.cursorPrevWordStartJa(editor);
+            assert.equal(13, editor.selection.active.character);
+        });
+    });
 });
